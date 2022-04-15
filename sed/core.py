8527820ccf540125280ab433d8e999afd8add6fc
rettigl <l.rettig@web.de>
2022-04-15 20:39:43 +0200

    evenHistogram function adapted from mpes

--- sed/core.py
+++ sed/core.py
@@ -1,10 +1,15 @@
 from typing import Any
+from typing import Union
+from typing import Tuple
+from typing import Sequence
+from typing import List
 
 import pandas as pd
 import psutil
 import xarray as xr
 
 from .metadata import MetaHandler
+from .diagnostic import grid_histogram
 
 N_CPU = psutil.cpu_count()
 
@@ -15,6 +20,11 @@ class SedProcessor:
     def __init__(self):
 
         self._dataframe = None
+
+        self._config={}
+        self._config["default_bins"] = [80, 80, 80, 80]
+        self._config["default_axes"] = ['X', 'Y', 't', 'ADC']
+        self._config["default_ranges"] = [(0, 1800), (0, 1800), (68000, 74000), (0, 500)]
 
         self._dimensions = []
         self._coordinates = {}
@@ -74,7 +84,7 @@ class SedProcessor:
         Returns:
             None
         """
-        self._dataframe = pd.DataFrame(data)
+        self._dataframe = data
 
     def compute(
         self,
@@ -107,6 +117,65 @@ class SedProcessor:
             [description]
         """
         pass
+
+    def viewEventHistogram(self, dfpid: int, ncol: int=2, bins: Sequence[int]=None, 
+                axes:Sequence[str]=None,
+                ranges:Sequence[Tuple[float, float]]=None, jittered:bool = True,
+                backend:str='bokeh', legend:bool=True, histkwds:dict={}, legkwds:dict={}, **kwds:Any):
+        """
+        Plot individual histograms of specified dimensions (axes) from a substituent dataframe partition.
+        **Parameters**\n
+        dfpid: int
+            Number of the data frame partition to look at.
+        ncol: int
+            Number of columns in the plot grid.
+        axes: list/tuple
+            Name of the axes to view.
+        bins: list/tuple
+            Bin values of all speicified axes.
+        ranges: list
+            Value ranges of all specified axes.
+        backend: str | 'matplotlib'
+            Backend of the plotting library ('matplotlib' or 'bokeh').
+        legend: bool | True
+            Option to include a legend in the histogram plots.
+        histkwds, legkwds, **kwds: dict, dict, keyword arguments
+            Extra keyword arguments passed to ``mpes.visualization.grid_histogram()``.
+        """
+        if bins is None:
+            bins = self._config["default_bins"]
+        if axes is None:
+            axes = self._config["default_axes"]
+        if ranges is None:
+            ranges = self._config["default_ranges"]
+
+        input_types = map(type, [axes, bins, ranges])
+        allowed_types = [list, tuple]
+
+        df = self._dataframe
+
+        if jittered:
+            assert self._dataframe_jittered is not None, (
+                "jittered dataframe needs to be generated first, "
+                "use SedProcessor.gen_jittered_df(cols)!"
+            )
+            df = self._dataframe_jittered
+
+        if set(input_types).issubset(allowed_types):
+
+            # Read out the values for the specified groups
+            group_dict = {}
+            dfpart = df.get_partition(dfpid)
+            cols = dfpart.columns
+            for ax in axes:
+                group_dict[ax] = dfpart.values[:, cols.get_loc(ax)].compute()
+
+            # Plot multiple histograms in a grid
+            grid_histogram(group_dict, ncol=ncol, rvs=axes, rvbins=bins, rvranges=ranges,
+                    backend=backend, legend=legend, histkwds=histkwds, legkwds=legkwds, **kwds)
+
+        else:
+            raise TypeError('Inputs of axes, bins, ranges need to be list or tuple!')
 
     def add_dimension(self, name, range):
         if name in self._coordinates:
--- sed/diagnostic.py
+++ sed/diagnostic.py
@@ -1,1 +1,116 @@
-# TODO: populate
+import bokeh.plotting as pbk
+from bokeh.io import output_notebook
+from bokeh.layouts import gridplot
+import numpy as np
+import matplotlib.pyplot as plt
+
+from typing import List
+from typing import Tuple
+from typing import Sequence
+from typing import Any
+
+def plot_single_hist(histvals:List[int], edges:List[float], legend:bool=None, **kwds:Any):
+    """ Bokeh-based plotting of a single histogram with legend and tooltips.
+    **Parameters**\n
+    histvals: 1D array
+        Histogram counts (e.g. vertical axis).
+    edges: 1D array
+        Histogram edge values (e.g. horizontal axis).
+    legend: str
+        Text for the plot legend.
+    **kwds:
+        Keyword arguments for 'bokeh.plotting.figure().quad()'.
+    **Return**\n
+    p: object
+        An instance of 'bokeh.plotting.figure()' as a plot handle.
+    """
+
+    ttp = kwds.pop('tooltip', [('(x, y)', '($x, $y)')])
+
+    p = pbk.figure(background_fill_color='white', tooltips=ttp)
+    p.quad(top=histvals, bottom=0, left=edges[:-1], right=edges[1:],
+           line_color='white', alpha=0.8, legend=legend, **kwds)
+
+    p.y_range.start = 0
+    p.legend.location = 'top_right'
+    p.grid.grid_line_color = 'lightgrey'
+
+    return p
+
+def grid_histogram(dct:dict, ncol:int, rvs:List, rvbins:List,
+          rvranges:Sequence[Tuple[float, float]], backend:str='bokeh',
+          legend:bool=True, histkwds:dict={}, legkwds:dict={}, **kwds:Any):
+    """
+    Grid plot of multiple 1D histograms.
+    **Parameters**\n
+    dct: dict
+        Dictionary containing the name and values of the random variables.
+    ncol: int
+        Number of columns in the plot grid.
+    rvs: list/tuple
+        List of names for the random variables (rvs).
+    rvbins: list/tuple
+        Bin values for all random variables.
+    rvranges: list/tuple
+        Value ranges of all random variables.
+    backend: str | 'bokeh'
+        Backend for making the plot ('matplotlib' or 'bokeh').
+    legend: bool | True
+        Option to include a legend in each histogram plot.
+    histkwds: dict | {}
+        Keyword arguments for histogram plots.
+    legkwds: dict | {}
+        Keyword arguments for legends.
+    **kwds: keyword arguments
+    """
+
+    figsz = kwds.pop('figsize', (14, 8))
+
+    if backend == 'matplotlib':
+
+        nrv = len(rvs)
+        nrow = int(np.ceil(nrv / ncol))
+        histtype = kwds.pop('histtype', 'step')
+
+        f, ax = plt.subplots(nrow, ncol, figsize=figsz)
+        otherax = ax.copy()
+        for i, zipped in enumerate(zip(rvs, rvbins, rvranges)):
+
+            # Make each histogram plot
+            rvname, rvbin, rvrg = zipped
+            try:
+                axind = np.unravel_index(i, (nrow, ncol))
+                ax[axind].hist(dct[rvname], bins=rvbin, range=rvrg, label=rvname, histtype=histtype, **histkwds)
+                if legend == True:
+                    ax[axind].legend(fontsize=15, **legkwds)
+
+                otherax[axind] = None
+
+            except:
+                ax[i].hist(dct[rvname], bins=rvbin, range=rvrg, label=rvname, histtype=histtype, **histkwds)
+                if legend == True:
+                    ax[i].legend(fontsize=15, **legkwds)
+
+                otherax[i] = None
+
+        for oax in otherax.flatten():
+            if oax is not None:
+                f.delaxes(oax)
+
+    elif backend == 'bokeh':
+
+        output_notebook(hide_banner=True)
+
+        plots = []
+        for i, zipped in enumerate(zip(rvs, rvbins, rvranges)):
+
+            rvname, rvbin, rvrg = zipped
+            histvals, edges = np.histogram(dct[rvname], bins=rvbin, range=rvrg)
+
+            if legend == True:
+                plots.append(plot_single_hist(histvals, edges, legend=rvname, **histkwds))
+            else:
+                plots.append(plot_single_hist(histvals, edges, legend=None, **histkwds))
+
+        # Make grid plot
+        pbk.show(gridplot(plots, ncols=ncol, plot_width=figsz[0]*30, plot_height=figsz[1]*28))
