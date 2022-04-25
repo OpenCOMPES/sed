1ca3a8ebffd55f26d755c49b942baabfad4e8ef1
rettigl <l.rettig@web.de>
2022-04-26 01:15:43 +0200

    changed to only use one dataframe and overwrite columns with jittering, and remove configuration parameters from default interface

--- sed/core.py
+++ sed/core.py
@@ -31,8 +31,18 @@ class SedProcessor:
         self._config = config
         if not isinstance(self._config, dict):
             self._config = {}
+        if "hist_mode" not in self._config.keys():
+            self._config["hist_mode"] = "numba"
+        if "mode" not in self._config.keys():
+            self._config["mode"] = "fast"
+        if "pbar" not in self._config.keys():
+            self._config["pbar"] = True
         if "num_cores" not in self._config.keys():
             self._config["num_cores"] = N_CPU - 1
+        if "threads_per_worker" not in self._config.keys():
+            self._config["threads_per_worker"] = 4
+        if "threadpool_API" not in self._config.keys():
+            self._config["threadpool_API"] = "blas"
 
         self._dataframe = df
         self._dataframe_jittered = None
@@ -97,9 +107,9 @@ class SedProcessor:
         """
         self._dataframe = pd.DataFrame(data)
 
-    def gen_jittered_df(self, cols: Sequence[str] = None) -> None:
-        """Generate a a second dataframe, where the selected dataframe
-         columns have jitter applied
+    def add_jitter(self, cols: Sequence[str] = None) -> None:
+        """Add jitter to the selected dataframe columns.
+
 
         Args:
             cols: the colums onto which to apply jitter. If omitted,
@@ -114,7 +124,7 @@ class SedProcessor:
             except KeyError:
                 cols = self._dataframe.columns  # jitter all columns
 
-        self._dataframe_jittered = self._dataframe.map_partitions(
+        self._dataframe = self._dataframe.map_partitions(
             apply_jitter,
             cols=cols,
             cols_jittered=cols,
@@ -132,58 +142,61 @@ class SedProcessor:
         ] = 100,
         axes: Union[str, Sequence[str]] = None,
         ranges: Sequence[Tuple[float, float]] = None,
-        histMode: str = "numba",
-        mode: str = "fast",
-        jittered: bool = True,
-        pbar: bool = True,
-        nCores: int = None,
-        nThreadsPerWorker: int = 4,
-        threadpoolAPI: str = "blas",
         **kwds,
     ) -> xr.DataArray:
         """Compute the histogram along the given dimensions.
 
         Args:
-            mode: Binning method, choose between numba,
-                fast, lean and legacy (Y. Acremann's method).
-            ncores: [description].
-            axes: [description].
-            nbins: [description].
-            ranges: [description].
-            pbar: [description].
-            jittered: [description].
-            pbenv: [description].
+            bins: Definition of the bins. Can  be any of the following cases:
+                - an integer describing the number of bins in on all dimensions
+                - a tuple of 3 numbers describing start, end and step of the binning
+                  range
+                - a np.arrays defining the binning edges
+                - a list (NOT a tuple) of any of the above (int, tuple or np.ndarray)
+                - a dictionary made of the axes as keys and any of the above as values.
+                This takes priority over the axes and range arguments.
+            axes: The names of the axes (columns) on which to calculate the histogram.
+                The order will be the order of the dimensions in the resulting array.
+            ranges: list of tuples containing the start and end point of the binning
+                    range.
+            kwds: Keywords argument passed to bin_dataframe.
+
+        Raises:
+            AssertError: Rises when no dataframe has been loaded.
 
         Returns:
-            [description]
+            The result of the n-dimensional binning represented in an
+                xarray object, combining the data with the axes.
         """
+
         assert (
             self._dataframe is not None
         ), "dataframe needs to be loaded first!"
 
-        if nCores is None:
-            nCores = self._config["num_cores"]
-
-        df = self._dataframe
-
-        if jittered:
-            assert self._dataframe_jittered is not None, (
-                "jittered dataframe needs to be generated first, "
-                "use SedProcessor.gen_jittered_df(cols)!"
-            )
-            df = self._dataframe_jittered
+        hist_mode = kwds.pop("hist_mode", self._config["hist_mode"])
+        mode = kwds.pop("mode", self._config["mode"])
+        pbar = kwds.pop("pbar", self._config["pbar"])
+        num_cores = kwds.pop("num_cores", self._config["num_cores"])
+        threads_per_worker = kwds.pop(
+            "threads_per_worker",
+            self._config["threads_per_worker"],
+        )
+        threadpool_API = kwds.pop(
+            "threadpool_API",
+            self._config["threadpool_API"],
+        )
 
         self._binned = bin_dataframe(
-            df=df,
+            df=self._dataframe,
             bins=bins,
             axes=axes,
             ranges=ranges,
-            histMode=histMode,
+            histMode=hist_mode,
             mode=mode,
             pbar=pbar,
-            nCores=nCores,
-            nThreadsPerWorker=nThreadsPerWorker,
-            threadpoolAPI=threadpoolAPI,
+            nCores=num_cores,
+            nThreadsPerWorker=threads_per_worker,
+            threadpoolAPI=threadpool_API,
             **kwds,
         )
         return self._binned
