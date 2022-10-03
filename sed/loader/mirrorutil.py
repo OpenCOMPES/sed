daf9f4e7d8f7fb6e5b6059b5d6328977db9327b6
rettigl <l.rettig@web.de>
2022-10-04 00:07:23 +0200

    linting according to pylint

--- sed/loader/mirrorutil.py
+++ sed/loader/mirrorutil.py
@@ -4,6 +4,7 @@ second (local) location.
 Mostly ported from https://github.com/mpes-kit/mpes.
 @author: L. Rettig
 """
+import errno
 import os
 import shutil
 from datetime import datetime
@@ -19,7 +20,7 @@ class CopyTool:
 
         self.source = source
         self.dest = dest
-        self.safetyMargin = kwds.pop(
+        self.safety_margin = kwds.pop(
             "safetyMargin",
             1 * 2**30,
         )  # Default 500 GB safety margin
@@ -32,18 +33,17 @@ class CopyTool:
         else:
             self.ntasks = int(ntasks)
 
-    def copy(
+    def copy(  # pylint: disable=R0912, R0914
         self,
         source,
-        forceCopy=False,
+        force_copy=False,
         scheduler="threads",
         **compute_kwds,
     ):
         """Local file copying method."""
 
         if not os.path.exists(source):
-            print("Source not found!")
-            return
+            raise FileNotFoundError("Source not found!")
 
         filenames = []
         dirnames = []
@@ -51,7 +51,7 @@ class CopyTool:
         if os.path.isfile(source):
             # Single file
             sdir = os.path.dirname(os.path.realpath(source))
-            ddir = getTargetDir(
+            ddir = get_target_dir(
                 sdir,
                 self.source,
                 self.dest,
@@ -63,7 +63,7 @@ class CopyTool:
 
         elif os.path.isdir(source):
             sdir = source
-            ddir = getTargetDir(
+            ddir = get_target_dir(
                 sdir,
                 self.source,
                 self.dest,
@@ -75,105 +75,114 @@ class CopyTool:
             for path, dirs, files in os.walk(sdir):
                 for file in files:
                     filenames.append(os.path.join(path, file))
-                for dir in dirs:
-                    dirnames.append(os.path.join(path, dir))
+                for directory in dirs:
+                    dirnames.append(os.path.join(path, directory))
+
+        if not filenames:
+            raise FileNotFoundError("No files found at path!")
 
         # actual copy loop
-        if filenames:
-            # Check space left
-            size_src = 0
-            size_dst = 0
-            for sfile in filenames:
-                size_src += os.path.getsize(sfile)
-                if os.path.exists(sfile.replace(sdir, ddir)):
-                    size_dst += os.path.getsize(sfile.replace(sdir, ddir))
-            if size_src == 0 and not forceCopy:
-                # nothing to copy, just return directory
-                return ddir
+        # Check space left
+        size_src = 0
+        size_dst = 0
+        for sfile in filenames:
+            size_src += os.path.getsize(sfile)
+            if os.path.exists(sfile.replace(sdir, ddir)):
+                size_dst += os.path.getsize(sfile.replace(sdir, ddir))
+        if size_src == 0 and not force_copy:
+            # nothing to copy, just return directory
+            return ddir
+        free = shutil.disk_usage(ddir).free
+        if size_src - size_dst > free - self.safety_margin:
+            raise OSError(
+                errno.ENOSPC,
+                "Target disk full, only "
+                + str(free / 2**30)
+                + " GB free, but "
+                + str((size_src - size_dst) / 2**30)
+                + " GB needed!",
+            )
+
+        # make directories
+        for directory in dirnames:
+            dest_dir = directory.replace(sdir, ddir)
+            mymakedirs(dest_dir, gid=self.gid, mode=0o775)
+
+        copy_tasks = []  # Core-level jobs
+        for src_file in filenames:
+            dest_file = src_file.replace(sdir, ddir)
+            size_src = os.path.getsize(src_file)
+            if os.path.exists(dest_file):
+                size_dst = os.path.getsize(dest_file)
             else:
-                total, used, free = shutil.disk_usage(ddir)
-                if size_src - size_dst > free - self.safetyMargin:
-                    print(
-                        "Target disk full, only "
-                        + str(free / 2**30)
-                        + " GB free, but "
-                        + str((size_src - size_dst) / 2**30)
-                        + " GB needed!",
+                size_dst = 0
+            if (
+                not os.path.exists(dest_file)
+                or size_dst != size_src
+                or force_copy
+            ):
+                if os.path.exists(dest_file):
+                    # delete existing file, to fix permission issue
+                    copy_tasks.append(
+                        d.delayed(mycopy)(
+                            src_file,
+                            dest_file,
+                            gid=self.gid,
+                            mode=0o664,
+                            replace=True,
+                        ),
                     )
-                    return
-
-            # make directories
-            for directory in dirnames:
-                destDir = directory.replace(sdir, ddir)
-                mymakedirs(destDir, gid=self.gid, mode=0o775)
-
-            copyTasks = []  # Core-level jobs
-            for srcFile in filenames:
-                destFile = srcFile.replace(sdir, ddir)
-                size_src = os.path.getsize(srcFile)
-                if os.path.exists(destFile):
-                    size_dst = os.path.getsize(destFile)
                 else:
-                    size_dst = 0
-                if (
-                    not os.path.exists(destFile)
-                    or size_dst != size_src
-                    or forceCopy
-                ):
-                    if os.path.exists(destFile):
-                        # delete existing file, to fix permission issue
-                        copyTasks.append(
-                            d.delayed(mycopy)(
-                                srcFile,
-                                destFile,
-                                gid=self.gid,
-                                mode=0o664,
-                                replace=True,
-                            ),
-                        )
-                    else:
-                        copyTasks.append(
-                            d.delayed(mycopy)(
-                                srcFile,
-                                destFile,
-                                gid=self.gid,
-                                mode=0o664,
-                            ),
-                        )
-
-            # run the copy tasks
-            if len(copyTasks) > 0:
-                print("Copy Files...")
-                with ProgressBar():
-                    d.compute(
-                        *copyTasks,
-                        scheduler=scheduler,
-                        num_workers=self.ntasks,
-                        **compute_kwds,
+                    copy_tasks.append(
+                        d.delayed(mycopy)(
+                            src_file,
+                            dest_file,
+                            gid=self.gid,
+                            mode=0o664,
+                        ),
                     )
-                print("Copy finished!")
 
-            if os.path.isfile(source):
-                return destFile
-            elif os.path.isdir(source):
-                return ddir
+        # run the copy tasks
+        if len(copy_tasks) > 0:
+            print("Copy Files...")
+            with ProgressBar():
+                d.compute(
+                    *copy_tasks,
+                    scheduler=scheduler,
+                    num_workers=self.ntasks,
+                    **compute_kwds,
+                )
+            print("Copy finished!")
+
+        if os.path.isdir(source):
+            return ddir
+
+        return dest_file
 
     def size(self, sdir):
         """Calculate file size."""
 
-        for path, dirs, filenames in os.walk(sdir):
+        for path, dirs, filenames in os.walk(  # pylint: disable=W0612
+            sdir,
+        ):
             # Check space left
             size = 0
             for sfile in filenames:
                 size += os.path.getsize(os.path.join(sdir, sfile))
             return size
 
-    def cleanUpOldestScan(self, force=False, report=False):
+    def cleanup_oldest_scan(  # pylint: disable=R0912
+        self,
+        force=False,
+        report=False,
+    ):
         """Remove scans in old directories."""
 
         # get list of all Scan directories (leaf directories)
-        scan_dirs = list()
-        for root, dirs, files in os.walk(self.dest):
+        scan_dirs = []
+        for root, dirs, files in os.walk(  # pylint: disable=W0612
+            self.dest,
+        ):
             if not dirs:
                 scan_dirs.append(root)
 
@@ -185,7 +194,9 @@ class CopyTool:
             total_size = 0
             for scan in scan_dirs:
                 size = 0
-                for path, dirs, filenames in os.walk(scan):
+                for path, dirs, filenames in os.walk(  # pylint: disable=W0612
+                    scan,
+                ):
                     for sfile in filenames:
                         size += os.path.getsize(os.path.join(scan, sfile))
                 total_size += size
@@ -195,21 +206,22 @@ class CopyTool:
                         f"{(size/2**30):.2f} GB,     {scan}",
                     )
             print(f"Total size: {(total_size/2**30):.2f} GB.")
-        oldestScan = None
+        oldest_scan = None
         for scan in scan_dirs:
             size = 0
-            for path, dirs, filenames in os.walk(scan):
+            for path, dirs, filenames in os.walk(  # pylint: disable=W0612
+                scan,
+            ):
                 for sfile in filenames:
                     size += os.path.getsize(os.path.join(scan, sfile))
             if size > 0:
-                oldestScan = scan
+                oldest_scan = scan
                 break
-        if oldestScan is None:
-            print("No scan with data found to remove!")
-            return
+        if oldest_scan is None:
+            raise FileNotFoundError("No scan with data found to remove!")
 
         print(
-            f'Oldest scan is "{oldestScan}", removing it will free ',
+            f'Oldest scan is "{oldest_scan}", removing it will free ',
             f"{(size/2**30):.2f} GB space.",
         )
         if force:
@@ -218,35 +230,40 @@ class CopyTool:
             print("Proceed (y/n)?")
             proceed = input()
         if proceed == "y":
-            shutil.rmtree(oldestScan)
+            shutil.rmtree(oldest_scan)
             print("Removed sucessfully!")
         else:
             print("Aborted.")
 
 
 # private Functions
-def getTargetDir(sdir, source, dest, gid, mode, create=False):
+def get_target_dir(
+    sdir,
+    source,
+    dest,
+    gid,
+    mode,
+    create=False,
+):  # pylint: disable=R0913
     """Retrieve target directories."""
 
     if not os.path.isdir(sdir):
-        print("Only works for Directories!")
-        return
+        raise NotADirectoryError("Only works for directories!")
 
     dirs = []
     head, tail = os.path.split(sdir)
     dirs.append(tail)
     while not os.path.samefile(head, source):
         if os.path.samefile(head, "/"):
-            print("sdir needs to be inside of source!")
-            return
+            raise ValueError("sdir needs to be inside of source!")
 
         head, tail = os.path.split(head)
         dirs.append(tail)
 
     dirs.reverse()
     ddir = dest
-    for dir in dirs:
-        ddir = os.path.join(ddir, dir)
+    for directory in dirs:
+        ddir = os.path.join(ddir, directory)
         if create and not os.path.exists(ddir):
             mymakedirs(ddir, mode, gid)
     return ddir
@@ -258,7 +275,7 @@ def mymakedirs(path, mode, gid):
 
     if not path or os.path.exists(path):
         return []
-    (head, tail) = os.path.split(path)
+    (head, tail) = os.path.split(path)  # pylint: disable=W0612
     res = mymakedirs(head, mode, gid)
     os.mkdir(path)
     os.chmod(path, mode)
