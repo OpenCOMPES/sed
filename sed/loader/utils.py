c5958eead4ac22a3984ed79ca42d20956a76d3b8
M. Zain Sohail <zainsohail04@gmail.com>
2023-01-26 11:20:51 +0100

    utils for flash loader

--- sed/loader/utils.py
+++ sed/loader/utils.py
@@ -3,6 +3,7 @@
 from glob import glob
 from typing import cast
 from typing import List
+from pathlib import Path
 
 from natsort import natsorted
 
@@ -43,3 +44,36 @@ def gather_files(
         raise
 
     return files
+
+def gather_flash_files(
+        run_number: int, daq: str, raw_data_dir: str, extension: str = "h5",
+    ) -> List[Path]:
+    """Returns all filenames of given run located in directory
+    for the given daq."""
+    stream_name_prefixes = {
+        "pbd": "GMD_DATA_gmd_data",
+        "pbd2": "FL2PhotDiag_pbd2_gmd_data",
+        "fl1user1": "FLASH1_USER1_stream_2",
+        "fl1user2": "FLASH1_USER2_stream_2",
+        "fl1user3": "FLASH1_USER3_stream_2",
+        "fl2user1": "FLASH2_USER1_stream_2",
+        "fl2user2": "FLASH2_USER2_stream_2",
+    }
+
+    return sorted(Path(raw_data_dir).glob( f"{stream_name_prefixes[daq]}_run{run_number}_*."
+                + extension), key=lambda filename: str(filename).rsplit("_", maxsplit=1)[-1])
+
+def parse_h5_keys(h5_file, prefix=""):
+    """Helper method which parses the channels present in the h5 file"""
+    file_channel_list = []
+
+    for key in h5_file.keys():
+        try:
+            [
+                file_channel_list.append(s)
+                for s in parse_h5_keys(h5_file[key], prefix=prefix + "/" + key)
+            ]
+        except Exception:
+            file_channel_list.append(prefix + "/" + key)
+
+    return file_channel_list
