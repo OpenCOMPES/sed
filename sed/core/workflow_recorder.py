2c87a507d371f8e2a3f63448f31cef9393a31eec
M. Zain Sohail <zainsohail04@gmail.com>
2023-02-01 04:37:57 +0100

    Keep state of all functions that hold this decorator

--- sed/core/workflow_recorder.py
+++ sed/core/workflow_recorder.py
@@ -1,20 +1,32 @@
 """Module contains a call tracker and a dataclass for saving the method calls."""
-from functools import wraps
 from dataclasses import dataclass
+from functools import update_wrapper
+from typing import List
+
 
 @dataclass
 class MethodCall:
-    """A dataclass that records a method call with it's name, args and any additional kwargs."""
+    """A dataclass that records a method call with it's name,
+    args and any additional kwargs."""
+
     name: str
-    args: list
+    args: tuple
     kwargs: dict
 
-def track_call(func):
-    """A decorator that records each call to the decorated function."""
-    @wraps(func)
-    def wrapper(self, *args, **kwargs):
-        self._call_tracker.append(MethodCall(func.__name__, args, kwargs))
 
-        return func(self, *args, **kwargs)
+class CallTracker:
+    """A decorator that records each call with its arguments to the
+    decorated function. The call_tracker is a class attribute and can
+    be accessed by the class name, and holds the list of all calls."""
 
-    return wrapper
+    call_tracker: List[MethodCall] = []
+
+    def __init__(self, func):
+        update_wrapper(self, func)
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        self.__class__.call_tracker.append(
+            MethodCall(self.func.__name__, args, kwargs),
+        )
+        return self.func(*args, **kwargs)
