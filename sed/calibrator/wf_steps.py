64dfbcbef9bcd4877c364d215ad6e6c46ab2bb84
Steinn Ymir Agustsson <agustsson@phys.au.dk>
2023-03-02 09:50:56 +0100

    Add Jitter step

--- sed/calibrator/wf_steps.py
+++ sed/calibrator/wf_steps.py
@@ -90,7 +90,7 @@ class DLDSectorCorrection(WorkflowStep):
         )
 
 
-class AddColumns(WorkflowStep):
+class SumColumns(WorkflowStep):
     def __init__(
         self,
         col_a: str,
@@ -191,3 +191,46 @@ class DivideColumns(WorkflowStep):
         assert self.col_a in df.columns
         assert self.col_b in df.columns
         return df[self.col_a] / df[self.col_b]
+
+
+class AddJitter(WorkflowStep):
+    def __init__(
+        self,
+        column: str,
+        amplitude: float = 0.5,
+        jitter_type: str = "uniform",
+        inplace: bool = True,
+        out_cols: Union[str, Sequence[str]] = None,
+        duplicate_policy: str = "append",
+        notes: str = "",
+    ) -> None:
+        if out_cols is None:
+            out_cols = column if inplace else f"{column}_jit"
+        super().__init__(out_cols, duplicate_policy, notes)
+        assert jitter_type in [
+            "uniform",
+            "normal",
+        ], f"jitter type must be one of 'uniform' and 'normal', not {jitter_type}"
+        self.jitter_type = jitter_type
+        self.amplitude = amplitude
+        self.column = column
+
+    @property
+    def name(self):
+        return f"AddJitter_{self.column}"
+
+    @property
+    def jitter_array(self):
+        if self.jitter_type == "uniform":
+            # Uniform Jitter distribution
+            return np.random.uniform(low=-1, high=1, size=self._col_size)
+        elif self.jitter_type == "normal":
+            # Normal Jitter distribution works better for non-linear transformations and
+            # jitter sizes that don't match the original bin sizes
+            return np.random.standard_normal(size=self._col_size)
+
+    def func(self, df: ddf.DataFrame) -> ddf.DataFrame:
+        self._col_size = df.shape[0]
+        # print(self._col_size,df)
+
+        return df[self.column] + self.jitter_array * self.amplitude
