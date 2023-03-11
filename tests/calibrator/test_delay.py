34f2a108d3879c70f45115ebfb5335a5c11e4fe6
rettigl <l.rettig@web.de>
2023-03-11 14:11:29 +0100

    Add claibration dictionary and better metadata handling for DelayCalibrator Improve and extend tests for DelayCalibrator

--- tests/calibrator/test_delay.py
+++ tests/calibrator/test_delay.py
@@ -20,27 +20,68 @@ def test_delay_parameters_from_file():
         files=[file],
     )
     dc = DelayCalibrator(config=config)
-    dc.append_delay_axis(df, datafile=file)
+    df, metadata = dc.append_delay_axis(df, datafile=file)
     assert "delay" in df.columns
+    assert "datafile" in metadata["calibration"]
+    assert "delay_range" in metadata["calibration"]
+    assert "adc_range" in metadata["calibration"]
+    assert "time0" in metadata["calibration"]
+    assert "delay_range_mm" in metadata["calibration"]
 
 
 def test_delay_parameters_from_delay_range():
     """Test the option to extract the delay parameters from a delay range"""
+    # from keywords
     df, _ = get_loader(loader_name="mpes", config=config).read_dataframe(
         files=[file],
     )
     dc = DelayCalibrator(config=config)
-    dc.append_delay_axis(df, delay_range=(-100, 200))
+    df, metadata = dc.append_delay_axis(df, delay_range=(-100, 200))
     assert "delay" in df.columns
+    assert "delay_range" in metadata["calibration"]
+    assert "adc_range" in metadata["calibration"]
+
+    # from calibration
+    df, _ = get_loader(loader_name="mpes", config=config).read_dataframe(
+        files=[file],
+    )
+    dc = DelayCalibrator(config=config)
+    calibration = {"delay_range": (-100, 200), "adc_range": (100, 1000)}
+    df, metadata = dc.append_delay_axis(df, calibration=calibration)
+    assert "delay" in df.columns
+    assert "delay_range" in metadata["calibration"]
+    assert "adc_range" in metadata["calibration"]
+    assert metadata["calibration"]["adc_range"] == (100, 1000)
 
 
 def test_delay_parameters_from_delay_range_mm():
     """Test the option to extract the delay parameters from a mm range + t0"""
+    # from keywords
     df, _ = get_loader(loader_name="mpes", config=config).read_dataframe(
         files=[file],
     )
     dc = DelayCalibrator(config=config)
     with pytest.raises(NotImplementedError):
         dc.append_delay_axis(df, delay_range_mm=(1, 15))
-    dc.append_delay_axis(df, delay_range_mm=(1, 15), time0=1)
+    df, metadata = dc.append_delay_axis(df, delay_range_mm=(1, 15), time0=1)
     assert "delay" in df.columns
+    assert "delay_range" in metadata["calibration"]
+    assert "adc_range" in metadata["calibration"]
+    assert "time0" in metadata["calibration"]
+    assert "delay_range_mm" in metadata["calibration"]
+
+    # from dict
+    df, _ = get_loader(loader_name="mpes", config=config).read_dataframe(
+        files=[file],
+    )
+    dc = DelayCalibrator(config=config)
+    calibration = {"delay_range_mm": (1, 15)}
+    with pytest.raises(NotImplementedError):
+        dc.append_delay_axis(df, calibration=calibration)
+    calibration["time0"] = 1
+    df, metadata = dc.append_delay_axis(df, calibration=calibration)
+    assert "delay" in df.columns
+    assert "delay_range" in metadata["calibration"]
+    assert "adc_range" in metadata["calibration"]
+    assert "time0" in metadata["calibration"]
+    assert "delay_range_mm" in metadata["calibration"]
