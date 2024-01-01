bd6bb7e4f41711095c1d52c1acb55e0ccb661b66
rettigl <l.rettig@web.de>
2024-01-02 00:12:22 +0100

    rework delayCalibrator interface, and add creation time stamp generation

--- sed/calibrator/delay.py
+++ sed/calibrator/delay.py
@@ -1,6 +1,7 @@
 """sed.calibrator.delay module. Code for delay calibration.
 """
 from copy import deepcopy
+from datetime import datetime
 from typing import Any
 from typing import Dict
 from typing import List
@@ -53,7 +54,7 @@ class DelayCalibrator:
         df: Union[pd.DataFrame, dask.dataframe.DataFrame],
         adc_column: str = None,
         delay_column: str = None,
-        calibration: dict = None,
+        calibration: Dict[str, Any] = None,
         adc_range: Union[Tuple, List, np.ndarray] = None,
         delay_range: Union[Tuple, List, np.ndarray] = None,
         time0: float = None,
@@ -62,7 +63,7 @@ class DelayCalibrator:
         p1_key: str = None,
         p2_key: str = None,
         t0_key: str = None,
-        **kwargs,
+        verbose: bool = True,
     ) -> Tuple[Union[pd.DataFrame, dask.dataframe.DataFrame], dict]:
         """Calculate and append the delay axis to the events dataframe, by converting
         values from an analog-digital-converter (ADC).
@@ -93,6 +94,8 @@ class DelayCalibrator:
                 Defaults to config["delay"]["p2_key"]
             t0_key (str, optional): hdf5 key for t0 value (mm).
                 Defaults to config["delay"]["t0_key"]
+            verbose (bool, optional): Option to print out diagnostic information.
+                Defaults to True.
 
         Raises:
             ValueError: Raised if delay parameters are not found in the file.
@@ -102,28 +105,34 @@ class DelayCalibrator:
             Union[pd.DataFrame, dask.dataframe.DataFrame]: dataframe with added column
             and delay calibration metdata dictionary.
         """
-        if len(kwargs) > 0:
-            print(f"WARNING: arguments {kwargs.keys()} are not used in mpes delay calibration.")
         # pylint: disable=duplicate-code
         if calibration is None:
-            if self.calibration:
-                calibration = deepcopy(self.calibration)
-            else:
-                calibration = deepcopy(
-                    self._config["delay"].get(
-                        "calibration",
-                        {},
-                    ),
-                )
+            calibration = deepcopy(self.calibration)
 
-        if adc_range is not None:
-            calibration["adc_range"] = adc_range
-        if delay_range is not None:
-            calibration["delay_range"] = delay_range
-        if time0 is not None:
-            calibration["time0"] = time0
-        if delay_range_mm is not None:
-            calibration["delay_range_mm"] = delay_range_mm
+        if (
+            adc_range is not None
+            or delay_range is not None
+            or time0 is not None
+            or delay_range_mm is not None
+            or datafile is not None
+        ):
+            calibration = {}
+            calibration["creation_date"] = datetime.now().timestamp()
+            if adc_range is not None:
+                calibration["adc_range"] = adc_range
+            if delay_range is not None:
+                calibration["delay_range"] = delay_range
+            if time0 is not None:
+                calibration["time0"] = time0
+            if delay_range_mm is not None:
+                calibration["delay_range_mm"] = delay_range_mm
+        else:
+            # report usage of loaded parameters
+            if "creation_date" in calibration and verbose:
+                datestring = datetime.fromtimestamp(calibration["creation_date"]).strftime(
+                    "%m/%d/%Y, %H:%M:%S",
+                )
+                print(f"Using delay calibration parameters generated on {datestring}")
 
         if adc_column is None:
             adc_column = self.adc_column
@@ -158,7 +167,8 @@ class DelayCalibrator:
                     calibration["datafile"] = datafile
                     calibration["delay_range_mm"] = (ret[0], ret[1])
                     calibration["time0"] = ret[2]
-                    print(f"Extract delay range from file '{datafile}'.")
+                    if verbose:
+                        print(f"Extract delay range from file '{datafile}'.")
                 else:
                     raise NotImplementedError(
                         "Not enough parameters for delay calibration.",
@@ -170,7 +180,9 @@ class DelayCalibrator:
                     calibration["time0"],
                 ),
             )
-            print(f"delay_range (ps) = {calibration['delay_range']}")
+            if verbose:
+                print(f"Converted delay_range (ps) = {calibration['delay_range']}")
+            calibration["creation_date"] = datetime.now().timestamp()
 
         if "delay_range" in calibration.keys():
             df[delay_column] = calibration["delay_range"][0] + (
@@ -178,28 +190,38 @@ class DelayCalibrator:
             ) * (calibration["delay_range"][1] - calibration["delay_range"][0]) / (
                 calibration["adc_range"][1] - calibration["adc_range"][0]
             )
+            self.calibration = deepcopy(calibration)
+            if verbose:
+                print(
+                    "Append delay axis using delay_range = "
+                    f"[{calibration['delay_range'][0]}, {calibration['delay_range'][1]}]"
+                    " and adc_range = "
+                    f"[{calibration['adc_range'][0]}, {calibration['adc_range'][1]}]",
+                )
         else:
             raise NotImplementedError
 
         metadata = {"calibration": calibration}
-
         return df, metadata
 
     def add_offsets(
         self,
         df: dask.dataframe.DataFrame,
+        offsets: Dict[str, Any] = None,
         constant: float = None,
         flip_delay_axis: bool = None,
         columns: Union[str, Sequence[str]] = None,
-        weights: Union[float, Sequence[float]] = None,
+        weights: Union[float, Sequence[float]] = 1.0,
         preserve_mean: Union[bool, Sequence[bool]] = False,
         reductions: Union[str, Sequence[str]] = None,
         delay_column: str = None,
+        verbose: bool = True,
     ) -> Tuple[dask.dataframe.DataFrame, dict]:
         """Apply an offset to the delay column based on a constant or other columns.
 
         Args:
             df (Union[pd.DataFrame, dask.dataframe.DataFrame]): Dataframe to use.
+            offset (Dict, optional): Dictionary of delay offset parameters.
             constant (float, optional): The constant to shift the delay axis by.
             flip_delay_axis (bool, optional): Whether to flip the time axis. Defaults to False.
             columns (Union[str, Sequence[str]]): Name of the column(s) to apply the shift from.
@@ -211,27 +233,92 @@ class DelayCalibrator:
                 of dask.dataframe.Series. For example "mean". In this case the function is applied
                 to the column to generate a single value for the whole dataset. If None, the shift
                 is applied per-dataframe-row. Defaults to None. Currently only "mean" is supported.
+            verbose (bool, optional): Option to print out diagnostic information.
+                Defaults to True.
 
         Returns:
             dask.dataframe.DataFrame: Dataframe with the shifted delay axis.
             dict: Metadata dictionary.
         """
+        # pylint: disable=duplicate-code
+        if offsets is None:
+            offsets = deepcopy(self.offsets)
+
         if delay_column is None:
             delay_column = self.delay_column
+
         metadata: Dict[str, Any] = {
             "applied": True,
         }
 
-        if columns is None and constant is None:
-            # load from config
+        if columns is not None or constant is not None or flip_delay_axis:
+            # use passed parameters, overwrite config
+            offsets = {}
+            offsets["creation_date"] = datetime.now().timestamp()
+            # column-based offsets
+            if columns is not None:
+                if isinstance(weights, (int, float, np.integer, np.floating)):
+                    weights = [weights]
+                if len(weights) == 1:
+                    weights = [weights[0]] * len(columns)
+                elif not isinstance(weights, Sequence):
+                    raise TypeError(
+                        f"Invalid type for weights: {type(weights)}. Must be a number or sequence",
+                    )
+                if not all(isinstance(s, (int, float, np.integer, np.floating)) for s in weights):
+                    raise TypeError(
+                        f"Invalid type for weights: {type(weights)}. Must be a number or sequence",
+                    )
+
+                # pylint: disable=duplicate-code
+                if isinstance(columns, str):
+                    columns = [columns]
+                if isinstance(preserve_mean, bool):
+                    preserve_mean = [preserve_mean] * len(columns)
+                if not isinstance(reductions, Sequence):
+                    reductions = [reductions]
+                if len(reductions) == 1:
+                    reductions = [reductions[0]] * len(columns)
+
+                # store in offsets dictionary
+                for col, weight, pmean, red in zip(columns, weights, preserve_mean, reductions):
+                    offsets[col] = {
+                        "weight": weight,
+                        "preserve_mean": pmean,
+                        "reduction": red,
+                    }
+
+            # constant offset
+            if isinstance(constant, (int, float, np.integer, np.floating)):
+                offsets["constant"] = constant
+            elif constant is not None:
+                raise TypeError(f"Invalid type for constant: {type(constant)}")
+            # flip the time direction
+            if flip_delay_axis:
+                offsets["flip_delay_axis"] = flip_delay_axis
+
+        elif "creation_date" in offsets and verbose:
+            datestring = datetime.fromtimestamp(offsets["creation_date"]).strftime(
+                "%m/%d/%Y, %H:%M:%S",
+            )
+            print(f"Using delay offset parameters generated on {datestring}")
+
+        if len(offsets) > 0:
+            # unpack dictionary
             # pylint: disable=duplicate-code
             columns = []
             weights = []
             preserve_mean = []
             reductions = []
-            for k, v in self.offsets.items():
+            if verbose:
+                print("Delay offset parameters:")
+            for k, v in offsets.items():
+                if k == "creation_date":
+                    continue
                 if k == "constant":
                     constant = v
+                    if verbose:
+                        print(f"   Constant: {constant} ")
                 elif k == "flip_delay_axis":
                     fda = str(v)
                     if fda.lower() in ["true", "1"]:
@@ -242,77 +329,54 @@ class DelayCalibrator:
                         raise ValueError(
                             f"Invalid value for flip_delay_axis in config: {flip_delay_axis}.",
                         )
+                    if verbose:
+                        print(f"   Flip delay axis: {flip_delay_axis} ")
                 else:
                     columns.append(k)
                     try:
-                        weights.append(v["weight"])
-                    except KeyError as exc:
-                        raise KeyError(f"Missing weight for offset column {k} in config.") from exc
-                    preserve_mean.append(v.get("preserve_mean", False))
-                    reductions.append(v.get("reduction", None))
+                        weight = v["weight"]
+                    except KeyError:
+                        weight = 1
+                    weights.append(weight)
+                    pm = v.get("preserve_mean", False)
+                    preserve_mean.append(pm)
+                    red = v.get("reduction", None)
+                    reductions.append(red)
+                    if verbose:
+                        print(
+                            f"   Column[{k}]: Weight={weight}, Preserve Mean: {pm}, ",
+                            f"Reductions: {red}.",
+                        )
 
-        # apply offset
-        if columns is not None:
-            # use passed parameters
-            if isinstance(weights, (int, float, np.integer, np.floating)):
-                weights = [weights]
-            elif not isinstance(weights, Sequence):
-                raise TypeError(
-                    f"Invalid type for weights: {type(weights)}. Must be a number or sequence",
+            if len(columns) > 0:
+                df = dfops.offset_by_other_columns(
+                    df=df,
+                    target_column=delay_column,
+                    offset_columns=columns,
+                    weights=weights,
+                    preserve_mean=preserve_mean,
+                    reductions=reductions,
                 )
-            if not all(isinstance(s, (int, float, np.integer, np.floating)) for s in weights):
-                raise TypeError(
-                    f"Invalid type for weights: {type(weights)}. Must be a number or sequence",
+                metadata["delay_column"] = delay_column
+                metadata["columns"] = columns
+                metadata["weights"] = weights
+                metadata["preserve_mean"] = preserve_mean
+                metadata["reductions"] = reductions
+
+            if constant:
+                df[delay_column] = df.map_partitions(
+                    # flip sign if binding energy scale
+                    lambda x: x[delay_column] + constant,
+                    meta=(delay_column, np.float64),
                 )
+                metadata["constant"] = constant
 
-            df = dfops.offset_by_other_columns(
-                df=df,
-                target_column=delay_column,
-                offset_columns=columns,
-                weights=weights,
-                preserve_mean=preserve_mean,
-                reductions=reductions,
-            )
-            metadata["delay_column"] = delay_column
-            metadata["columns"] = columns
-            metadata["weights"] = weights
-            metadata["preserve_mean"] = preserve_mean
-            metadata["reductions"] = reductions
-            # pylint: disable=duplicate-code
-            if not isinstance(columns, Sequence):
-                columns = [columns]
-            if not isinstance(weights, Sequence):
-                weights = [weights]
-            if isinstance(preserve_mean, bool):
-                preserve_mean = [preserve_mean] * len(columns)
-            if not isinstance(reductions, Sequence):
-                reductions = [reductions]
-            if len(reductions) == 1:
-                reductions = [reductions[0]] * len(columns)
+            if flip_delay_axis:
+                df[delay_column] = -df[delay_column]
+                metadata["flip_delay_axis"] = True
 
-            for col, weight, pmean, red in zip(columns, weights, preserve_mean, reductions):
-                self.offsets[col] = {
-                    "weight": weight,
-                    "preserve_mean": pmean,
-                    "reduction": red,
-                }
+            self.offsets = offsets
 
-        # apply constant
-        if isinstance(constant, (int, float, np.integer, np.floating)):
-            df[delay_column] = df.map_partitions(
-                # flip sign if binding energy scale
-                lambda x: x[delay_column] + constant,
-                meta=(delay_column, np.float64),
-            )
-            metadata["constant"] = constant
-            self.offsets["constant"] = constant
-        elif constant is not None:
-            raise TypeError(f"Invalid type for constant: {type(constant)}")
-        # flip the time direction
-        if flip_delay_axis:
-            df[delay_column] = -df[delay_column]
-            metadata["flip_delay_axis"] = True
-            self.offsets["flip_delay_axis"] = True
         return df, metadata
 
 
