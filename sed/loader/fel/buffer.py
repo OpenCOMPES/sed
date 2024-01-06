4d950dbb42a8e3f4ae10406b458ea4729bd173ea
Zain Sohail <zainsohail04@gmail.com>
2024-01-06 21:29:08 +0100

    cleaning up the classes

--- sed/loader/fel/buffer.py
+++ sed/loader/fel/buffer.py
@@ -25,9 +25,11 @@ from joblib import Parallel
 from sed.core.dfops import forward_fill_lazy
 from sed.loader.fel.dataframe import DataFrameCreator
 from sed.loader.fel.parquet import ParquetHandler
+from sed.loader.fel.utils import get_channels
+from sed.loader.utils import split_dld_time_from_sector_id
 
 
-class BufferFileHandler(DataFrameCreator):
+class BufferFileHandler:
     """
     A class for handling the creation and manipulation of buffer files using DataFrameCreator
     and ParquetHandler.
@@ -76,10 +78,7 @@ class BufferFileHandler(DataFrameCreator):
 
         self.get_files_to_read(h5_paths, force_recreate)
 
-        if debug:
-            self.serial_buffer_file_creation()
-        else:
-            self.parallel_buffer_file_creation()
+        self.create_buffer_files(debug)
 
         self.get_filled_dataframe()
 
@@ -92,7 +91,10 @@ class BufferFileHandler(DataFrameCreator):
         """
         existing_parquet_filenames = [file for file in self.buffer_paths if file.exists()]
         parquet_schemas = [pq.read_schema(file) for file in existing_parquet_filenames]
-        config_schema = set(self.get_channels(formats="all", index=True, extend_aux=True))
+        config_schema = set(
+            get_channels(self._config["channels"], formats="all", index=True, extend_aux=True),
+        )
+
         if self._config.get("split_sector_id_from_dld_time", False):
             config_schema.add(self._config.get("sector_id_column", False))
 
@@ -135,35 +137,26 @@ class BufferFileHandler(DataFrameCreator):
 
         print(f"Reading files: {self.num_files} new files of {len(h5_paths)} total.")
 
-    def serial_buffer_file_creation(self) -> None:
+    def create_buffer_files(self, debug: bool) -> None:
         """
-        Serializes the creation of buffer files.
+        Creates the buffer files.
 
-        Raises:
-            ValueError: If an error occurs during the conversion process.
+        Args:
+            debug (bool): Flag to enable debug mode, which serializes the creation.
         """
-        dataframes: list[ddf.DataFrame] = None
+        dataframes: list[ddf.DataFrame] = []
         if self.num_files > 0:
-            dataframes = [self.create_dataframe_per_file(h5_path) for h5_path in self.h5_to_create]
-
-            for df, prq in zip(dataframes, self.buffer_to_create):
-                df.to_parquet(prq)
-
-    def parallel_buffer_file_creation(self) -> None:
-        """
-        Parallelizes the creation of buffer files.
-
-        Raises:
-            ValueError: If an error occurs during the conversion process.
-        """
-        dataframes: list[ddf.DataFrame] = None
-        if self.num_files > 0:
+            df_creator_instances = [
+                DataFrameCreator(self._config, h5_path) for h5_path in self.h5_to_create
+            ]
+        if debug:
+            dataframes = [df_creator.df for df_creator in df_creator_instances]
+        else:
             dataframes = Parallel(n_jobs=self.num_files, verbose=10)(
-                delayed(self.create_dataframe_per_file)(h5_path) for h5_path in self.h5_to_create
+                delayed(df_creator.df) for df_creator in df_creator_instances
             )
-
-            for df, prq in zip(dataframes, self.buffer_to_create):
-                df.to_parquet(prq)
+        for df, prq in zip(dataframes, self.buffer_to_create):
+            df.to_parquet(prq)
 
     def get_filled_dataframe(self) -> None:
         """
@@ -172,8 +165,12 @@ class BufferFileHandler(DataFrameCreator):
         dataframe = ddf.read_parquet(self.buffer_paths, calculate_divisions=True)
         metadata = [pq.read_metadata(file) for file in self.buffer_paths]
 
-        channels: list[str] = self.get_channels(["per_pulse", "per_train"], extend_aux=True)
-
+        channels: list[str] = get_channels(
+            self._config["channels"],
+            ["per_pulse", "per_train"],
+            extend_aux=True,
+        )
+        index: list[str] = get_channels(index=True)
         overlap = min(file.num_rows for file in metadata)
 
         print("Filling nan values...")
@@ -184,8 +181,16 @@ class BufferFileHandler(DataFrameCreator):
             iterations=self._config.get("forward_fill_iterations", 2),
         )
 
-        self.dataframe_electron = dataframe.dropna(
-            subset=self.get_channels(["per_electron"]),
-        )
-        self.dataframe_pulse = dataframe[self.multi_index + channels]
-        self.dataframe_pulse = self.dataframe_pulse[(self.dataframe_pulse["electronId"] == 0)]
+        # Drop rows with nan values in the tof column
+        tof_column = self._config.get("tof_column", "dldTimeSteps")
+        dataframe_electron = dataframe.dropna(subset=self._config.get(tof_column))
+
+        # Correct the 3-bit shift which encodes the detector ID in the 8s time
+        if self._config.get("split_sector_id_from_dld_time", False):
+            self.dataframe_electron = split_dld_time_from_sector_id(
+                dataframe_electron,
+                config=self._config,
+            )
+        else:
+            self.dataframe_electron = dataframe_electron
+        self.dataframe_pulse = dataframe[index + channels]
---
+++ sed/loader/fel/utils.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+
+MULTI_INDEX = ["trainId", "pulseId", "electronId"]
+PULSE_ALIAS = MULTI_INDEX[1]
+DLD_AUX_ALIAS = "dldAux"
+DLDAUX_CHANNELS = "dldAuxChannels"
+
+
+def get_channels(
+    channel_dict: dict = None,
+    formats: str | list[str] = None,
+    index: bool = False,
+    extend_aux: bool = False,
+) -> list[str]:
+    """
+    Returns a list of channels associated with the specified format(s).
+
+    Args:
+        formats (Union[str, List[str]]): The desired format(s)
+        ('per_pulse', 'per_electron', 'per_train', 'all').
+        index (bool): If True, includes channels from the multiindex.
+        extend_aux (bool): If True, includes channels from the 'dldAuxChannels' dictionary,
+                else includes 'dldAux'.
+
+    Returns:
+        List[str]: A list of channels with the specified format(s).
+    """
+    # If 'formats' is a single string, convert it to a list for uniform processing.
+    if isinstance(formats, str):
+        formats = [formats]
+
+    # If 'formats' is a string "all", gather all possible formats.
+    if formats == ["all"]:
+        channels = get_channels(
+            channel_dict,
+            ["per_electron", "per_pulse", "per_train"],
+            index,
+            extend_aux,
+        )
+        return channels
+
+    channels = []
+
+    # Include channels from multi_index if 'index' is True.
+    if index:
+        channels.extend(MULTI_INDEX)
+
+    if formats:
+        # If 'formats' is a list, check if all elements are valid.
+        for format_ in formats:
+            if format_ not in ["per_electron", "per_pulse", "per_train", "all"]:
+                raise ValueError(
+                    "Invalid format. Please choose from 'per_electron', 'per_pulse',\
+                    'per_train', 'all'.",
+                )
+
+        # Get the available channels excluding 'pulseId'.
+        available_channels = list(channel_dict.keys())
+        # raises error if not available, but necessary for pulse_index
+        available_channels.remove(PULSE_ALIAS)
+
+        for format_ in formats:
+            # Gather channels based on the specified format(s).
+            channels.extend(
+                key
+                for key in available_channels
+                if channel_dict[key]["format"] == format_ and key != DLD_AUX_ALIAS
+            )
+            # Include 'dldAuxChannels' if the format is 'per_pulse' and extend_aux is True.
+            # Otherwise, include 'dldAux'.
+            if format_ == "per_train" and DLD_AUX_ALIAS in available_channels:
+                if extend_aux:
+                    channels.extend(
+                        channel_dict[DLD_AUX_ALIAS][DLDAUX_CHANNELS].keys(),
+                    )
+                else:
+                    channels.extend([DLD_AUX_ALIAS])
+
+    return channels
