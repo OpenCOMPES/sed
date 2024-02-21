155ac9d782c40b2710f59a93c7691cb498416cc7
Zain Sohail <zainsohail04@gmail.com>
2024-02-21 18:07:59 +0100

    add test file for metadata

---
+++ tests/loader/flash/test_flash_metadata.py
@@ -0,0 +1,56 @@
+import pytest
+
+from sed.loader.flash.metadata import MetadataRetriever
+
+
+@pytest.fixture
+def mock_requests(requests_mock):
+    # Mocking the response for the dataset URL
+    dataset_url = "https://example.com/Datasets/11013410%2F43878"
+    requests_mock.get(dataset_url, json={"fake": "data"}, status_code=200)
+
+
+# Test cases for MetadataRetriever
+def test_get_metadata(mock_requests):
+    metadata_config = {
+        "scicat_url": "https://example.com",
+        "scicat_token": "fake_token",
+    }
+    retriever = MetadataRetriever(metadata_config)
+    metadata = retriever.get_metadata("11013410", ["43878"])
+    assert isinstance(metadata, dict)
+    assert metadata == {"fake": "data"}
+
+
+def test_get_metadata_with_existing_metadata(mock_requests):
+    metadata_config = {
+        "scicat_url": "https://example.com",
+        "scicat_token": "fake_token",
+    }
+    retriever = MetadataRetriever(metadata_config)
+    existing_metadata = {"existing": "metadata"}
+    metadata = retriever.get_metadata("11013410", ["43878"], existing_metadata)
+    assert isinstance(metadata, dict)
+    assert metadata == {"existing": "metadata", "fake": "data"}
+
+
+def test_get_metadata_per_run(mock_requests):
+    metadata_config = {
+        "scicat_url": "https://example.com",
+        "scicat_token": "fake_token",
+    }
+    retriever = MetadataRetriever(metadata_config)
+    metadata = retriever._get_metadata_per_run("11013410/43878")
+    assert isinstance(metadata, dict)
+    assert metadata == {"fake": "data"}
+
+
+def test_create_dataset_url_by_PID():
+    metadata_config = {
+        "scicat_url": "https://example.com",
+        "scicat_token": "fake_token",
+    }
+    retriever = MetadataRetriever(metadata_config)
+    url = retriever._create_dataset_url_by_PID("11013410/43878")
+    assert isinstance(url, str)
+    assert url == "https://example.com/Datasets/11013410%2F43878"
