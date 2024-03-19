a2cef165cd293c6dc933c4b9912eec9f4885395e
rettigl <l.rettig@web.de>
2024-03-19 12:28:51 +0100

    update target

--- benchmarks/benchmark_sed.py
+++ benchmarks/benchmark_sed.py
@@ -31,7 +31,7 @@ target_artificial_1d = 2.68
 target_artificial_4d = 8.15
 target_inv_dfield = 6.1
 target_binning_1d = 16.6
-target_binning_4d = 20.0
+target_binning_4d = 22.0
 
 
 def test_artificial_1d() -> None:
