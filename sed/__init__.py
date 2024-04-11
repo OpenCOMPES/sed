b49eded5e3304db44eba133ca616c34a5ac5fbdb
Zain Sohail <zainsohail04@gmail.com>
2024-04-11 12:56:29 +0200

    get version from toml version

--- sed/__init__.py
+++ sed/__init__.py
@@ -1,6 +1,10 @@
 """sed module easy access APIs
 
 """
+import toml
+
 from .core.processor import SedProcessor
 
+config = toml.load("pyproject.toml")
+__version__ = config["tool"]["poetry"]["version"]
 __all__ = ["SedProcessor"]
