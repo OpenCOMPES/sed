e6f3943c4d263d16cf2da594f1075f13b1be7b70
Zain Sohail <zainsohail04@gmail.com>
2024-04-11 13:15:54 +0200

    fix version

--- sed/__init__.py
+++ sed/__init__.py
@@ -1,10 +1,6 @@
 """sed module easy access APIs
 
 """
-import toml
-
 from .core.processor import SedProcessor
 
-config = toml.load("pyproject.toml")
-__version__ = config["tool"]["poetry"]["version"]
 __all__ = ["SedProcessor"]
