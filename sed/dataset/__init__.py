00b011b33c7f49bf54ee74196aa4af2a69977437
Zain Sohail <zainsohail04@gmail.com>
2024-05-12 17:20:46 +0200

    add user config dirs etc.

---
+++ sed/dataset/__init__.py
@@ -0,0 +1,7 @@
+from .dataset import available_datasets
+from .dataset import fetch
+
+__all__ = [
+    "fetch",
+    "available_datasets",
+]
--- sed/dataset/dataset.py
+++ sed/dataset/dataset.py
@@ -1,15 +1,23 @@
 import json
-import logging
 import os
 import shutil
 import zipfile
 
 import requests
 
-# Configure logging
-logging.basicConfig(filename="dataset_fetch.log", level=logging.INFO)
+from sed.core.config import setup_logging
+from sed.core.config import user_dirs
 
-DATASETS = json.load(open(os.path.join(os.path.dirname(__file__), "datasets.json")))
+# Configure logging
+logger = setup_logging()
+
+# check if datasets.json exists in user_config_dir
+if not os.path.exists(os.path.join(user_dirs.user_config_dir, "datasets.json")):
+    shutil.copy(
+        os.path.join(os.path.dirname(__file__), "datasets.json"),
+        os.path.join(user_dirs.user_config_dir, "datasets.json"),
+    )
+DATASETS = json.load(open(os.path.join(user_dirs.user_config_dir, "datasets.json")))
 
 
 def available_datasets():
@@ -17,7 +25,7 @@ def available_datasets():
     return list(DATASETS.keys())
 
 
-def fetch(data_name, data_path="."):
+def fetch(data_name: str, data_path: str = None):
     """
     Fetches the specified data and extracts it to the given data path.
 
@@ -26,51 +34,82 @@ def fetch(data_name, data_path="."):
         data_path (str): Path where the data should be stored. Default is the current directory.
     """
     if data_name not in DATASETS:
-        logging.error(f"Data '{data_name}' is not available for fetching.")
+        logger.error(
+            f"Data '{data_name}' is not available for fetching.\
+                Available datasets are: {available_datasets()}",
+        )
         return
 
     dataset_info = DATASETS[data_name]
     data_url = dataset_info["url"]
     subdirs = dataset_info.get("subdirs", [])
-    resort_files = dataset_info.get("resort_files", False)
+    rearrange_files = dataset_info.get("rearrange_files", False)
+    existing_data_path = dataset_info.get("data_path", "")
+    processed = dataset_info.get("processed", False)
+
+    ## tell user that data might already exists
+    if existing_data_path != data_path and data_path is not None:
+        logger.info(
+            f'{data_name} data might already exists at "{existing_data_path}",\
+            unless deleted manually.',
+        )
+
+    if data_path is None:
+        data_path = existing_data_path or user_dirs.user_data_dir
+        logger.info(f'Data path not provided. Using path: "{data_path}"')
+
     zip_file_path = os.path.join(data_path, f"{data_name}.zip")
 
     # Check if data already exists
     if not os.path.exists(zip_file_path):
-        logging.info(f"Downloading {data_name} data...")
+        logger.info(f"Downloading {data_name} data...")
         response = requests.get(data_url)
         with open(zip_file_path, "wb") as f:
             f.write(response.content)
-        logging.info("Download complete.")
-        downloaded = True
+        logger.info("Download complete.")
+        processed = False
     else:
-        logging.info(f"{data_name} data is already downloaded.")
-        downloaded = False
+        logger.info(f"{data_name} data is already downloaded.")
 
-    # Extract data if not already extracted
+    # Set Extract data flag to true, if not already extracted
     extract = True
     for subdir in subdirs:
         if os.path.isdir(os.path.join(data_path, subdir)):
             extract = False
 
     if extract:
-        logging.info(f"Extracting {data_name} data...")
+        processed = False
+        logger.info(f"Extracting {data_name} data...")
         with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
             zip_ref.extractall(data_path)
-        logging.info("Extraction complete.")
+        logger.info("Extraction complete.")
     else:
-        logging.info(f"{data_name} data is already extracted.")
+        logger.info(f"{data_name} data is already extracted.")
 
     # Move files if specified
-    if resort_files and downloaded:
+    if rearrange_files and not processed:
         for subdir in subdirs:
             source_path = os.path.join(data_path, subdir)
             if os.path.isdir(source_path):
-                logging.info(f"Moving files from {subdir} to {data_path}...")
+                logger.info(f"Rearranging files...")
                 for root, dirs, files in os.walk(source_path):
                     for file in files:
                         shutil.move(os.path.join(root, file), data_path)
-                # shutil.rmtree(source_path)
-                logging.info("File movement complete.")
+                logger.info("File movement complete.")
             else:
-                logging.error(f"Source directory {subdir} not found.")
+                logger.error(f"Subdirectory {subdir} not found.")
+        logger.info("Rearranging complete.")
+
+    ## add processed flag to dataset_info
+    processed = True
+    dataset_info["processed"] = processed
+    dataset_info["data_path"] = data_path
+    ## update datasets.json
+    with open(os.path.join(user_dirs.user_config_dir, "datasets.json"), "w") as f:
+        json.dump(DATASETS, f, indent=4)
+
+    # if rearrange_files is not true and subdirs are present, return the subdir path
+    if subdirs and not rearrange_files:
+        return [os.path.join(data_path, subdir) for subdir in subdirs]
+    else:
+        return data_path
