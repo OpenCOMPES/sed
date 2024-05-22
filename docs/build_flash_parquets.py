8a6f319ec9644e53658386a938ed75b5dcbef9c1
Zain Sohail <zainsohail04@gmail.com>
2024-05-22 22:37:21 +0200

    update tutorials

--- docs/build_flash_parquets.py
+++ docs/build_flash_parquets.py
@@ -1,30 +1,20 @@
-import os
 from pathlib import Path
 
 import sed
 from sed import SedProcessor
+from sed.dataset import load_dataset
 
 config_file = Path(sed.__file__).parent / "config/flash_example_config.yaml"
-data_path = "./tutorial/"  # Put in Path to a storage of at least 20 Gbyte free space.
-if not os.path.exists(data_path + "/Gd_W110_flash.zip"):
-    os.system(
-        f"curl -L --output {data_path}/Gd_W110_flash.zip https://zenodo.org/records/10658470/files/single_event_data.zip",
-    )
-if not os.path.isdir(data_path + "/flash_data"):
-    if not os.path.isdir(data_path + "/analysis_data") or not os.path.isdir(
-        data_path + "/calibration_data",
-    ):
-        os.system(f"unzip -d {data_path} -o {data_path}/Gd_W110_flash.zip")
-
-    os.system(f"mkdir {data_path}/flash_data")
-    os.system(f"mv {data_path}/analysis_data/*/*.h5 {data_path}/flash_data")
-    os.system(f"mv {data_path}/calibration_data/*/*.h5 {data_path}/flash_data")
+data_path = load_dataset(
+    "Gd_W(110)",
+    "./tutorial/",
+)  # Put in Path to a storage of at least 20 Gbyte free space.
 
 config_override = {
     "core": {
         "paths": {
-            "data_raw_dir": data_path + "/flash_data/",
-            "data_parquet_dir": data_path + "/processed/",
+            "data_raw_dir": data_path,
+            "data_parquet_dir": str(data_path) + "/processed/",
         },
     },
 }
