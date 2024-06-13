c1fa34ff25be5374cea1124a8818e63128f9ce5b
Zain Sohail <zainsohail04@gmail.com>
2024-06-13 17:06:17 +0200

    try doc build

--- docs/scripts/build_flash_parquets.py
+++ docs/scripts/build_flash_parquets.py
@@ -6,7 +6,7 @@ from sed.dataset import dataset
 
 config_file = Path(sed.__file__).parent / "config/flash_example_config.yaml"
 
-dataset.get("Gd_W110", root_dir=Path(sed.__file__).parent / "docs/tutorial/", remove_zip=False)
+dataset.get("Gd_W110")
 data_path = dataset.dir
 
 
