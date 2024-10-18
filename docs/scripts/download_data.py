e69e081f1278038a724b0b964d819f7ffe1e4ec7
rettigl <l.rettig@web.de>
2024-10-18 23:12:10 +0200

    add sxp tutorial to documentation

--- docs/scripts/download_data.py
+++ docs/scripts/download_data.py
@@ -5,3 +5,4 @@ root_dir = "./tutorial"
 dataset.get("WSe2", remove_zip=True, root_dir=root_dir)
 dataset.get("Gd_W110", remove_zip=True, root_dir=root_dir)
 dataset.get("TaS2", remove_zip=True, root_dir=root_dir)
+dataset.get("Au_Mica", remove_zip=True, root_dir=root_dir)
