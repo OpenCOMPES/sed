8186e9a41ff0b546c622500ceeb6c74ae940aaf5
rettigl <l.rettig@web.de>
2025-01-16 15:34:24 +0100

    remove pump beam section if pump closed

--- src/sed/loader/mpes/metadata.py
+++ src/sed/loader/mpes/metadata.py
@@ -328,6 +328,11 @@ class MetadataRetriever:
             elif metadata["elabFTW"]["scan"]["probe_polarization"] == "p":
                 metadata["elabFTW"]["scan"]["probe_polarization"] = 0
 
+        # remove pump information if pump not applied:
+        if not metadata["elabFTW"]["scan"].get("pump_status", 0):
+            if "pump_photon_energy" in metadata["elabFTW"].get("laser_status", {}):
+                del metadata["elabFTW"]["laser_status"]["pump_photon_energy"]
+
         return metadata
 
 
