373950538add4d06ef3abb08a25950f27f67f060
M. Zain Sohail <zainsohail04@gmail.com>
2025-04-13 00:15:36 +0200

    separated lab loading procedure but using common methods from flash loader

---
+++ src/sed/loader/cfel/buffer_handler.py
@@ -0,0 +1,104 @@
+from __future__ import annotations
+
+import time
+from pathlib import Path
+
+import dask.dataframe as dd
+
+from sed.core.logging import setup_logging
+from sed.loader.cfel.dataframe import DataFrameCreator
+from sed.loader.flash.buffer_handler import BufferFilePaths
+from sed.loader.flash.buffer_handler import BufferHandler as BaseBufferHandler
+from sed.loader.flash.utils import get_channels
+from sed.loader.flash.utils import get_dtypes
+
+logger = setup_logging("cfel_buffer_handler")
+
+
+class BufferHandler(BaseBufferHandler):
+    """
+    A class for handling the creation and manipulation of buffer files using DataFrameCreator.
+    """
+
+    def __init__(
+        self,
+        config: dict,
+    ) -> None:
+        """
+        Initializes the BufferHandler.
+
+        Args:
+            config (dict): The configuration dictionary.
+        """
+        super().__init__(config)
+
+    def _save_buffer_file(self, paths: dict[str, Path]) -> None:
+        """Creates the electron and timed buffer files from the raw H5 file."""
+        logger.debug(f"Processing file: {paths['raw'].stem}")
+        start_time = time.time()
+
+        # Create DataFrameCreator and get get dataframe
+        dfc = DataFrameCreator(config_dataframe=self._config, h5_path=paths["raw"])
+        df = dfc.df
+
+        # Save electron resolved dataframe
+        electron_channels = get_channels(self._config, "per_electron")
+        dtypes = get_dtypes(self._config, df.columns.values)
+        electron_df = df.dropna(subset=electron_channels).astype(dtypes).reset_index()
+        logger.debug(f"Saving electron buffer with shape: {electron_df.shape}")
+        electron_df.to_parquet(paths["electron"])
+
+        # Create and save timed dataframe
+        df_timed = dfc.df_timed
+        dtypes = get_dtypes(self._config, df_timed.columns.values)
+        timed_df = df_timed.astype(dtypes)
+        logger.debug(f"Saving timed buffer with shape: {timed_df.shape}")
+        timed_df.to_parquet(paths["timed"])
+
+        logger.debug(f"Processed {paths['raw'].stem} in {time.time() - start_time:.2f}s")
+
+    def process_and_load_dataframe(
+        self,
+        h5_paths: list[Path],
+        folder: Path,
+        force_recreate: bool = False,
+        suffix: str = "",
+        debug: bool = False,
+        remove_invalid_files: bool = False,
+        filter_timed_by_electron: bool = True,
+    ) -> tuple[dd.DataFrame, dd.DataFrame]:
+        """
+        Runs the buffer file creation process.
+        Does a schema check on the buffer files and creates them if they are missing.
+        Performs forward filling and splits the sector ID from the DLD time lazily.
+
+        Args:
+            h5_paths (List[Path]): List of paths to H5 files.
+            folder (Path): Path to the folder for processed files.
+            force_recreate (bool): Flag to force recreation of buffer files.
+            suffix (str): Suffix for buffer file names.
+            debug (bool): Flag to enable debug mode.):
+            remove_invalid_files (bool): Flag to remove invalid files.
+            filter_timed_by_electron (bool): Flag to filter timed data by valid electron events.
+
+        Returns:
+            Tuple[dd.DataFrame, dd.DataFrame]: The electron and timed dataframes.
+        """
+        self.filter_timed_by_electron = filter_timed_by_electron
+        if remove_invalid_files:
+            h5_paths = self._validate_h5_files(self._config, h5_paths)
+
+        self.fp = BufferFilePaths(h5_paths, folder, suffix)
+
+        if not force_recreate:
+            schema_set = set(
+                get_channels(self._config, formats="all", index=True, extend_aux=True),
+            )
+            self._schema_check(self.fp["electron"], schema_set)
+            self._schema_check(self.fp["timed"], schema_set)
+
+        self._save_buffer_files(force_recreate, debug)
+
+        self._get_dataframes()
+
+        return self.df["electron"], self.df["timed"]
---
+++ src/sed/loader/cfel/dataframe.py
@@ -0,0 +1,213 @@
+"""
+This module creates pandas DataFrames from HDF5 files for different levels of data granularity
+[per electron, per pulse, and per train]. It efficiently handles concatenation of data from
+various channels within the HDF5 file, making use of the structured nature data to optimize
+join operations. This approach significantly enhances performance compared to earlier.
+"""
+from __future__ import annotations
+
+from pathlib import Path
+
+import h5py
+import numpy as np
+import pandas as pd
+
+from sed.core.logging import setup_logging
+from sed.loader.flash.utils import get_channels
+from sed.loader.flash.utils import InvalidFileError
+
+logger = setup_logging("cfel_dataframe_creator")
+
+
+class DataFrameCreator:
+    """
+    A class for creating pandas DataFrames from an HDF5 file for HEXTOF lab data at CFEL.
+
+    Attributes:
+        h5_file (h5py.File): The HDF5 file object.
+        multi_index (pd.MultiIndex): The multi-index structure for the DataFrame.
+        _config (dict): The configuration dictionary for the DataFrame.
+    """
+
+    def __init__(self, config_dataframe: dict, h5_path: Path) -> None:
+        """
+        Initializes the DataFrameCreator class.
+
+        Args:
+            config_dataframe (dict): The configuration dictionary with only the dataframe key.
+            h5_path (Path): Path to the h5 file.
+        """
+        self.h5_file = h5py.File(h5_path, "r")
+        self._config = config_dataframe
+
+        index_alias = self._config.get("index", ["countId"])[0]
+        # all values except the last as slow data starts from start of file
+        self.index = np.cumsum([0, *self.get_dataset_array(index_alias)])
+
+    def get_dataset_key(self, channel: str) -> str:
+        """
+        Checks if 'dataset_key' exists and returns that.
+
+        Args:
+            channel (str): The name of the channel.
+
+        Returns:
+            str: The 'dataset_key'.
+
+        Raises:
+            ValueError: If 'dataset_key' is not provided.
+        """
+        channel_config = self._config["channels"][channel]
+        if "dataset_key" in channel_config:
+            return channel_config["dataset_key"]
+        error = f"For channel: {channel}, provide 'dataset_key'."
+        raise ValueError(error)
+
+    def get_dataset_array(
+        self,
+        channel: str,
+    ) -> h5py.Dataset:
+        """
+        Returns a numpy array for a given channel name.
+
+        Args:
+            channel (str): The name of the channel.
+            slice_ (bool): Applies slicing on the dataset. Default is True.
+
+        Returns:
+            h5py.Dataset: The channel's data as a h5py.Dataset object.
+        """
+        # Get the data from the necessary h5 file and channel
+        dataset_key = self.get_dataset_key(channel)
+        dataset = self.h5_file[dataset_key]
+
+        return dataset
+
+    @property
+    def df_electron(self) -> pd.DataFrame:
+        """
+        Returns a pandas DataFrame for channel names of type [per electron].
+
+        Returns:
+            pd.DataFrame: The pandas DataFrame for the 'per_electron' channel's data.
+        """
+        # Get the relevant channels and their slice index
+        channels = get_channels(self._config, "per_electron")
+        if channels == []:
+            return pd.DataFrame()
+
+        series = {channel: pd.Series(self.get_dataset_array(channel)) for channel in channels}
+        dataframe = pd.concat(series, axis=1)
+        return dataframe.dropna()
+
+    @property
+    def df_train(self) -> pd.DataFrame:
+        """
+        Returns a pandas DataFrame for given channel names of type [per pulse].
+
+        Returns:
+            pd.DataFrame: The pandas DataFrame for the 'per_train' channel's data.
+        """
+        series = []
+        # Get the relevant channel names
+        channels = get_channels(self._config, "per_train")
+        # auxiliary dataset (which is stored in the same dataset as other DLD channels)
+        aux_alias = self._config.get("aux_alias", "dldAux")
+
+        # For each channel, a pd.Series is created and appended to the list
+        for channel in channels:
+            dataset = self.get_dataset_array(channel)
+
+            if channel == aux_alias:
+                try:
+                    sub_channels = self._config["channels"][aux_alias]["sub_channels"]
+                except KeyError:
+                    raise KeyError(
+                        f"Provide 'sub_channels' for auxiliary channel '{aux_alias}'.",
+                    )
+                for name, values in sub_channels.items():
+                    series.append(
+                        pd.Series(
+                            dataset[:, values["slice"]],
+                            self.index[:-1],
+                            name=name,
+                        ),
+                    )
+            else:
+                series.append(pd.Series(dataset, self.index[:-1], name=channel))
+        # All the channels are concatenated to a single DataFrame
+        return pd.concat(series, axis=1)
+
+    @property
+    def df_timestamp(self) -> pd.DataFrame:
+        """
+        Uses the first_event_time_stamp_key to get initial timestamp and the
+        ms_markers_key which is a dataset of exposure times same size as the index."""
+
+        first_tstamp = self.h5_file[self._config.get("first_event_time_stamp_key")][
+            0
+        ]  # single value
+        ts_start = pd.to_datetime(first_tstamp.decode())
+        # actually in seconds but using milliseconds for consistency with mpes loader
+        exposure_time = self.h5_file[self._config.get("ms_markers_key")][()]
+
+        # Calculate cumulative exposure times
+        cumulative_exposure = np.cumsum(exposure_time)
+        timestamps = [ts_start + pd.Timedelta(seconds=cum_exp) for cum_exp in cumulative_exposure]
+        # add initial timestamp to the start of the list
+        timestamps.insert(0, ts_start)
+        # Create a DataFrame with the timestamps
+        df = pd.DataFrame({"timestamp": timestamps}, index=self.index)
+
+        return df
+
+    def validate_channel_keys(self) -> None:
+        """
+        Validates if the index and dataset keys for all channels in the config exist in the h5 file.
+
+        Raises:
+            InvalidFileError: If the index or dataset keys are missing in the h5 file.
+        """
+        invalid_channels = []
+        for channel in self._config["channels"]:
+            dataset_key = self.get_dataset_key(channel)
+            if dataset_key not in self.h5_file:
+                invalid_channels.append(channel)
+
+        if invalid_channels:
+            raise InvalidFileError(invalid_channels)
+
+    @property
+    def df(self) -> pd.DataFrame:
+        """
+        Joins the 'per_electron', 'per_pulse', and 'per_train' using concat operation,
+        returning a single dataframe.
+
+        Returns:
+            pd.DataFrame: The combined pandas DataFrame.
+        """
+
+        self.validate_channel_keys()
+        df_train = self.df_train
+        df_timestamp = self.df_timestamp
+        df = pd.concat((self.df_electron, df_train, df_timestamp), axis=1)
+        ffill_cols = list(df_train.columns) + list(df_timestamp.columns)
+        df[ffill_cols] = df[ffill_cols].ffill()
+        df.index.name = self._config.get("index", ["countId"])[0]
+        return df
+
+    @property
+    def df_timed(self) -> pd.DataFrame:
+        """
+        Joins the 'per_electron', 'per_pulse', and 'per_train' using concat operation,
+        returning a single dataframe.
+
+        Returns:
+            pd.DataFrame: The combined pandas DataFrame.
+        """
+
+        self.validate_channel_keys()
+        df_train = self.df_train
+        df_timestamp = self.df_timestamp
+        df = pd.concat((self.df_electron, df_train, df_timestamp), axis=1, join="inner")
+        return df
