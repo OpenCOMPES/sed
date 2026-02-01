969181bc7abd313292703bebbd197a3132721ac5
rettigl <l.rettig@web.de>
2026-02-01 12:39:02 +0100

    update dask and pandas

--- src/sed/__init__.py
+++ src/sed/__init__.py
@@ -1,10 +1,6 @@
 """sed module easy access APIs."""
 import importlib.metadata
 
-import dask
-
-dask.config.set({"dataframe.query-planning": False})
-
 from .core.processor import SedProcessor  # noqa: E402
 
 __version__ = importlib.metadata.version("sed-processor")
