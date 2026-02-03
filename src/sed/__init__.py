2f53ced5705143914d812e2fdbb4b2aa582efa8c
rettigl <l.rettig@web.de>
2026-02-03 21:58:33 +0100

    revert dask version

--- src/sed/__init__.py
+++ src/sed/__init__.py
@@ -1,6 +1,10 @@
 """sed module easy access APIs."""
 import importlib.metadata
 
+import dask
+
+dask.config.set({"dataframe.query-planning": False})
+
 from .core.processor import SedProcessor  # noqa: E402
 
 __version__ = importlib.metadata.version("sed-processor")
